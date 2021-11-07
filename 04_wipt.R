37f2caa1f91a67c638cc200ca20d6750fbfd34b8
michaelagustafson <michaelagustafso@u.boisestate.edu>
2021-11-06 20:10:13 -0600

    fixed sky variable updated model to have obs and site covs

--- 04_wipt.R
+++ 04_wipt.R
@@ -25,6 +25,8 @@ library(unmarked)
 
 all.obs <- read.csv(here("data/all_obs.csv"))
 all.env <- read.csv(here("data/all_env.csv"))
+all.hab <- read.csv(here("data/all_hab.csv"))
+
 
 # get rid of extra 'X' column, this will be taken care of for obs
 # when aggregating time intervals and creating a wide data frame
@@ -109,8 +111,35 @@ wipt.wide <- wipt.agg %>%
   spread(key = time_int, value = count, fill = 0)
 
 
-str(all.env) 
+##### NEED TO CHANGE SKY VARIABLE TO HIGHEST NUMBER
+?grepl
+str(all.env)
 all.env$sky <- as.factor(all.env$sky)
+
+
+# going to have to use dsub and do it for each character string of multiple categories
+unique(all.env$sky)
+
+all.env$sky <- gsub("2,3,4", "4", all.env$sky)
+all.env$sky <- gsub("3,4", "4", all.env$sky)
+all.env$sky <- gsub("2,4", "4", all.env$sky)
+all.env$sky <- gsub("2,3", "3", all.env$sky)
+all.env$sky <- gsub("0,3", "3", all.env$sky)
+all.env$sky <- gsub("1,3", "3", all.env$sky)
+all.env$sky <- gsub("3,0", "3", all.env$sky)
+all.env$sky <- gsub("3,", "3", all.env$sky)
+
+unique(all.env$sky)
+
+all.env$sky <- as.factor(all.env$sky)
+
+
+# Combine extracted habitat variables with rest of environmental data
+
+# WILL NEED TO CHANGE THIS INNER JOIN WHEN I HAVE ALL POINTS
+all.env <- inner_join(all.env, all.hab)
+
+str(all.env) 
 all.env$year <- as.factor(all.env$year)
 all.env$observer <- as.factor(all.env$observer)
 
@@ -137,7 +166,7 @@ which(x == "TRUE", arr.ind = TRUE)
 # one of my temps is NA
 # looked at temps of surveys before (40) and after (44) and can enter a temp
 
-all.env[552,6] <- "42"
+all.env[534,6] <- "42"
 
 for( p in 1:length(prednames) ){
   # create an object with the ggplot so that you can display it 
@@ -156,6 +185,11 @@ jul.scaled <- scale(all.env$julian)
 min.scaled <- scale(all.env$min_after_sun)
 hear.scaled <- scale(all.env$hear)
 temp.scaled <- scale(as.numeric(all.env$tempf))
+t.scaled <- scale(as.numeric(all.env$Tundra))
+ls.scaled <- scale(as.numeric(all.env$Low_Shrub))
+ts.scaled <- scale(as.numeric(all.env$Tall_Shrub_Forest))
+bg.scaled <- scale(as.numeric(all.env$Bare_Ground))
+w.scaled <- scale(as.numeric(all.env$Wetlands))
 
 
 # replace in dataframe
@@ -165,19 +199,34 @@ all.env.scaled$julian <- jul.scaled
 all.env.scaled$min_after_sun <- min.scaled
 all.env.scaled$hear <- hear.scaled
 all.env.scaled$tempf <- temp.scaled
+all.env.scaled$Tundra <- t.scaled
+all.env.scaled$Low_Shrub <- ls.scaled
+all.env.scaled$Tall_Shrub_Forest <- ts.scaled
+all.env.scaled$Bare_Ground <- bg.scaled
+all.env.scaled$Wetlands <- w.scaled
+
 head(all.env.scaled)
 
 # look at correlations between scaled factors:
-cor(all.env.scaled$julian, all.env.scaled$tempf) # hmm 0.5 correlation... makes sense I guess? 
+cor(all.env.scaled$julian, all.env.scaled$tempf) # hmm 0.54 correlation... makes sense I guess? 
 # i think in my earlier analysis i used julian date instead of temp...
-cor(all.env.scaled$julian, all.env.scaled$min_after_sun) ### -0.18, not bad??
-cor(all.env.scaled$julian, all.env.scaled$hear) # -0.125
-cor(all.env.scaled$min_after_sun, all.env.scaled$hear) # 0.065
-cor(all.env.scaled$tempf, all.env.scaled$hear) #-0.097
-cor(all.env.scaled$tempf, all.env.scaled$min_after_sun)# 0.231
+cor(all.env.scaled$julian, all.env.scaled$min_after_sun) ### -0.196, not bad??
+cor(all.env.scaled$julian, all.env.scaled$hear) # -0.115
+cor(all.env.scaled$min_after_sun, all.env.scaled$hear) # 0.074
+cor(all.env.scaled$tempf, all.env.scaled$hear) #-0.092
+cor(all.env.scaled$tempf, all.env.scaled$min_after_sun)# 0.229
+cor(all.env.scaled$Tundra, all.env.scaled$Low_Shrub) #-0.29
+cor(all.env.scaled$Tundra, all.env.scaled$Tall_Shrub_Forest) #-0.5211585
+cor(all.env.scaled$Tundra, all.env.scaled$Bare_Ground) # -0.02976836
+cor(all.env.scaled$Tundra, all.env.scaled$Wetlands) #0.02119351
+cor(all.env.scaled$Low_Shrub, all.env.scaled$Tall_Shrub_Forest) # -0.6339179
+cor(all.env.scaled$Low_Shrub, all.env.scaled$Bare_Ground) #0.005949667
+cor(all.env.scaled$Low_Shrub, all.env.scaled$Wetlands) # -0.1943937
+cor(all.env.scaled$Tall_Shrub_Forest, all.env.scaled$Bare_Ground) #-0.1525284
+cor(all.env.scaled$Tall_Shrub_Forest, all.env.scaled$Wetlands) # -0.026575
+cor(all.env.scaled$Bare_Ground, all.env.scaled$Wetlands) # 0.1634681
 
-
-
+# high correlatiosn between tundra+tall shrub and tallshrub+lowshrub
 # correlations all seem good, probably going to continue to use julian instead of temp?
 
 ### RUN ANALYSIS
@@ -193,12 +242,14 @@ colnames(wipt.wide)
 
 timeints <- wipt.wide[,c(2:6)]
 
-siCovs <- all.env.scaled[,c(2, 4:8)]
+obcovs <- all.env.scaled[,c(2, 4:8)]
+siCovs <- all.env.scaled[,c(11:15)]
 
 
 wiptFrame2 <- unmarkedFrameMPois(
   # import time removal columns(counts):
   y = timeints, 
+  obsCovs = obsCovs,
   #import site level covariates:
   siteCovs = siCovs, # site covs will also be my spatial habitat data
   # define pifun type: 
