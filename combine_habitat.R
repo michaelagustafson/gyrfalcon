9b9d1e4282fbd805342c41c66cdf23ab57d43dc7
michaelagustafson <michaelagustafso@u.boisestate.edu>
2021-11-23 08:12:11 -0700

    replaced nlcd hab with above hab

--- combine_habitat.R
+++ combine_habitat.R
@@ -9,21 +9,25 @@ library(tidyverse)
 
 # Import data
 
-hab19 <- read.csv(here("data/points19_habitat.csv"))
-hab21 <- read.csv(here("data/points21_habitat.csv"))
+#hab19 <- read.csv(here("data/points19_habitat.csv"))
+#hab21 <- read.csv(here("data/points21_habitat.csv"))
 
 # bind to create one habitat df
 
-all.hab <- rbind(hab19, hab21)
-head(all.hab); dim(all.hab)
+#all.hab <- rbind(hab19, hab21)
+#head(all.hab); dim(all.hab)
 
-write.csv(all.hab, here("data/all_hab.csv"))
-
-
+#write.csv(all.hab, here("data/all_hab.csv"))
 
 
+# combine ABoVE habitat data
 
+hab19ab <- read.csv(here("data/points19_ABoVEhab.csv"))
+hab21ab <- read.csv(here("data/points21_ABoVEhab.csv"))
 
+above_hab_combined <- rbind(hab19ab, hab21ab)
+
+write.csv(above_hab_combined, here("data/above_hab_combined.csv"))
 
 
 
